93da101c68488046d7ecddfaac7926d979aa86ad
ctelfair20 <ctelfair20@gmail.com>
2021-08-29 00:01:26 -0500

    edited jobRoles event listener
    changes variable used to hide the other jobs input box
    
    added event listener to change the colorfield options depending on the theme

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 // Goal: add default focus to the name field
 
 // access name field
@@ -19,14 +21,44 @@ When a change is detected, display/hide the "text field" based on the user’s
 selection in the drop down menu. */
 
 // access the div with the id of basic-info-and-shirt-box
-const basicInfoBox = document.getElementsByClassName('basic-info-and-shirt-box')[0];
+const jobRoles = document.getElementById('title');
 // create a eventlistener for changes
-basicInfoBox.addEventListener('change', (e) => {
+jobRoles.addEventListener('change', (e) => {
     // check if the target has the tagname select
-    if (e.target.tagName === "SELECT") {
-        if (e.target.value === "other") {
-            otherJobField.style.display = 'inherit'
-        }
+    if (e.target.tagName === "SELECT" && e.target.value === "other") {
+        otherJobField.style.display = 'inherit';
+    }
+});
+
+
+/* Goal: The options in the "Color" drop down menu are not available for each t-shirt design. 
+So the user shouldn’t be able to see or choose a color option until they have chosen a design.*/
+
+
+// access the design dropdown menu
+const designField = document.getElementById('design');
+// access the color dropdown menu
+const colorField = document.getElementById('color');
+// if the value equals Select Theme
+if (designField.value === 'Select Theme') {
+    // disable colorField
+    colorField.disabled = true;
+// console.log(colorField.dataset.theme);
+}
+
+colorField.addEventListener('change', (e) => {
+    // iterate over color's children
+    for (let i = 0; i < colorField.children.length; i++) {
+        colorField.disabled = false;
+        console.log(colorField.children[i]);
+        // // if design's value equals theme js puns && if child has a class name of heart js
+        // if () {
+        //     // set display to none
+        
+        // // else if design's value equals theme heart js && if child has a class name of js puns
+        // } else if () {
+        //     // set display to none
+        // }   
     }
 });
 
